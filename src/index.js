2eb43b62dad1c4a2641a0cd362fea0747c9e2857
Diego <dgmontesg.12@gmail.com>
2022-03-14 21:44:38 -0700

    Init commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import '../src/css/normalize.css';
+import '../src/css/styles.css';
+import '../src/js/navbar.js';
---
+++ src/js/navbar.js
@@ -0,0 +1,11 @@
+const ToggleButton = document.querySelector(".toggle-button");
+const navBarLinks = document.querySelector(".ulist");
+
+
+ToggleButton.addEventListener("click",()=>{
+
+ navBarLinks.classList.toggle("active")
+
+
+
+});
